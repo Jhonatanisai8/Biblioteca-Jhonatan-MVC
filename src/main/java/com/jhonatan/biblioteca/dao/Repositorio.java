7fbc6b38790f078728aecfc649904854eeae9c59
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-16 11:26:00 -0500

    interface Repositorio de tipo generico con algunos metodos que despues implemetaremos

---
+++ src/main/java/com/jhonatan/biblioteca/dao/Repositorio.java
@@ -0,0 +1,14 @@
+package com.jhonatan.biblioteca.dao;
+
+import java.util.List;
+
+public interface Repositorio<T> {
+
+    List<T> listar();
+
+    T porId(Long id);
+
+    void guardar(T t);
+
+    void eliminar(T t);
+}

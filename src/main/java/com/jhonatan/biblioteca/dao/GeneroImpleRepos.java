c676daf183e0778f3da090260f9ab442a3153563
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-16 21:45:04 -0500

    clase GeneroImpleRepos: implementacion del metodo guardar y modifcacion del metodo crear-Genero

--- src/main/java/com/jhonatan/biblioteca/dao/GeneroImpleRepos.java
+++ src/main/java/com/jhonatan/biblioteca/dao/GeneroImpleRepos.java
@@ -3,6 +3,7 @@ package com.jhonatan.biblioteca.dao;
 import com.jhonatan.biblioteca.conexion.Conexion;
 import com.jhonatan.biblioteca.modelo.Genero;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,7 +14,8 @@ public class GeneroImpleRepos
         implements Repositorio<Genero> {
 
     private static final String SQL_SELECT = "SELECT id_genero,nombre_genero FROM genero";
-    
+    private static final String SQL_INSERT = "INSERT INTO genero(nombre_genero) VALUES (?)";
+    private static final String SQL_UPDATE = "UPDATE FROM genero SET nombre = ? WHERE id_genero = ?";
 
     //para la conexion 
     private Connection getConection() throws SQLException {
@@ -38,12 +40,6 @@ public class GeneroImpleRepos
         return generos;
     }
 
-    private Genero crearGenero(final ResultSet rs) throws SQLException {
-        Genero g = new Genero();
-        g.setIdGenero(rs.getInt("id_genero"));
-        g.setNombreGenero(rs.getString("nombre_genero"));
-        return g;
-    }
 
     @Override
     public Genero porId(Long id) {
@@ -52,7 +48,23 @@ public class GeneroImpleRepos
 
     @Override
     public void guardar(Genero t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String SQL;
+        if (t.getIdGenero() != null && t.getIdGenero() > 0) {
+            SQL = SQL_UPDATE;
+        } else {
+            SQL = SQL_INSERT;
+        }
+        try(Connection conn = getConection();
+            PreparedStatement st = conn.prepareStatement(SQL);){
+            st.setString(1, t.getNombreGenero());
+            if (t.getIdGenero() != null && t.getIdGenero() > 0) {
+                st.setLong(2, t.getIdGenero());
+            } 
+            st.executeUpdate();
+            
+        } catch(SQLException ex){
+            System.out.println("error en el metodo guardar: "+ex.getMessage());
+        }
     }
 
     @Override
@@ -60,4 +72,10 @@ public class GeneroImpleRepos
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    private Genero crearGenero(final ResultSet rs) throws SQLException {
+        Genero g = new Genero();
+        g.setIdGenero(rs.getLong("id_genero"));
+        g.setNombreGenero(rs.getString("nombre_genero"));
+        return g;
+    }
 }

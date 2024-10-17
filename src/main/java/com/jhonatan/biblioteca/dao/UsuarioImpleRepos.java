e62625b83e1e22022a5e4e8fa57895c1aff1badb
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-17 00:00:09 -0500

    clase UsuarioImpleRepos: implementacion del metodo guardar

--- src/main/java/com/jhonatan/biblioteca/dao/UsuarioImpleRepos.java
+++ src/main/java/com/jhonatan/biblioteca/dao/UsuarioImpleRepos.java
@@ -3,6 +3,7 @@ package com.jhonatan.biblioteca.dao;
 import com.jhonatan.biblioteca.conexion.Conexion;
 import com.jhonatan.biblioteca.modelo.Usuario;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,6 +14,9 @@ public class UsuarioImpleRepos
         implements Repositorio<Usuario> {
 
     private static final String SQL_SELECT = "SELECT id_usuario,nombre_usu,apellido1,apellido2,telefono,email,direccion FROM usuario";
+    private static final String SQL_SEARCH = "SELECT id_usuario,nombre_usu,apellido1,apellido2,telefono,email,direccion FROM usuario WHERE id_usuario = ?";
+    private static final String SQL_INSERT = "INSERT INTO usuario (nombre_usu,apellido1,apellido2,telefono,email,direccion) VALUES (?,?,?,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE usuario SET nombre_usu = ?,apellido1 = ? ,apellido2 = ? ,telefono = ? ,email = ?,direccion = ? WHERE id_usuario = ?";
 
     //para la conexion 
     private Connection getConection() throws SQLException {
@@ -35,12 +39,44 @@ public class UsuarioImpleRepos
 
     @Override
     public Usuario porId(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Usuario usuario = null;
+        try ( Connection con = getConection();  PreparedStatement st = con.prepareStatement(SQL_SEARCH);) {
+            st.setLong(1, id);
+            try ( ResultSet rs = st.executeQuery();) {
+                if (rs.next()) {
+                    usuario = crearUsuario(rs);
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println("error al listar usuario porID: " + ex.getMessage());
+        }
+        return usuario;
     }
 
     @Override
     public void guardar(Usuario t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String SQL;
+        if (t.getIdUsuario() != null && t.getIdUsuario() > 0) {
+            SQL = SQL_UPDATE;
+        } else {
+            SQL = SQL_INSERT;
+        }
+        try ( Connection con = getConection();  PreparedStatement st = con.prepareStatement(SQL);) {
+            st.setString(1, t.getNombre());
+            st.setString(2, t.getApellido1());
+            st.setString(3, t.getApellido2());
+            st.setString(4, t.getTelefono());
+            st.setString(5, t.getEmail());
+            st.setString(6, t.getDireccion());
+
+            if (t.getIdUsuario() != null && t.getIdUsuario() > 0) {
+                st.setLong(7, t.getIdUsuario());
+            }
+            st.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("error al guardar o modificar usuario: " + ex.getMessage());
+        }
+
     }
 
     @Override

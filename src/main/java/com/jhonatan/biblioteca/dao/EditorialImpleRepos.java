107369333fe82a4f84b3d97fb4d82c8bc4e18f73
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-16 22:51:19 -0500

    clase EditorialImpleRepos: implementacion del metodo porId

--- src/main/java/com/jhonatan/biblioteca/dao/EditorialImpleRepos.java
+++ src/main/java/com/jhonatan/biblioteca/dao/EditorialImpleRepos.java
@@ -3,6 +3,7 @@ package com.jhonatan.biblioteca.dao;
 import com.jhonatan.biblioteca.conexion.Conexion;
 import com.jhonatan.biblioteca.modelo.Editorial;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,8 +14,9 @@ public class EditorialImpleRepos
         implements Repositorio<Editorial> {
 
     private static final String SQL_SELECT = "SELECT id_editorial,nombre_edi,pais FROM editorial";
-    //para la conexion 
+    private static final String SQL_SEARCH = "SELECT id_editorial,nombre_edi,pais FROM editorial WHERE id_editorial =  ?";
 
+    //para la conexion 
     private Connection getConection() throws SQLException {
         return Conexion.getInstance();
     }
@@ -35,7 +37,18 @@ public class EditorialImpleRepos
 
     @Override
     public Editorial porId(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Editorial ed = null;
+        try ( Connection conn = getConection();  PreparedStatement st = conn.prepareStatement(SQL_SEARCH);) {
+            st.setLong(1, id);
+            try ( ResultSet rs = st.executeQuery();) {
+                if (rs.next()) {
+                    ed = crearEditorial(rs);
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println("error en poriD: " + ex.getMessage());
+        }
+        return ed;
     }
 
     @Override

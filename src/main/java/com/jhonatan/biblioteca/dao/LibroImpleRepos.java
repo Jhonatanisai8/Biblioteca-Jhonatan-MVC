34421f70d281317d1fc6c7376ec712a8cdb4e71b
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-21 01:10:37 -0500

    clase libro-implementacion-repos: harrmos todos los metodos para la insercion,listado de libros, eliminacion de libros, ect

---
+++ src/main/java/com/jhonatan/biblioteca/dao/LibroImpleRepos.java
@@ -0,0 +1,29 @@
+package com.jhonatan.biblioteca.dao;
+
+import com.jhonatan.biblioteca.modelo.Libro;
+import java.util.List;
+
+public class LibroImpleRepos
+        implements Repositorio<Libro> {
+
+    @Override
+    public List<Libro> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Libro porId(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void guardar(Libro t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
